1ff193adeb1e9d3eb2583aa280d5d9927aba8ef0
hsminu <hsminu@kakao.com>
2026-03-27 11:55:40 +0900

    commit

--- code/2-4/2-4.c
+++ code/2-4/2-4.c
@@ -7,7 +7,7 @@ int main() {
 
     for(int i=0; i< n; i++){
         char ch = chars[i];
-        printf("%c - alpha:%d digit:%d upper:%c lower%c\n",
+        printf("%c - alpha:%d digit:%d upper:%c lower:%c\n",
         ch, isalpha(ch), isdigit(ch), toupper(ch), tolower(ch));
     }
 
